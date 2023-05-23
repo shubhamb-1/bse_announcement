356f43278935ed70c75d3a58ef743018b84b9b0b
shubhamb-1 <bhardwajshubham843@gmail.com>
2023-05-23 19:48:57 +0530

    Dockerfile added

--- scheduler.py
+++ scheduler.py
@@ -4,6 +4,8 @@ from scrap import fetch_new_data
 scheduler = BackgroundScheduler()
 
 def scheduler_function():
-    scheduler.add_job(fetch_new_data, 'interval', seconds=30)
-    scheduler.start()
+    jobs = scheduler.get_jobs()
+    if not jobs:
+        scheduler.add_job(fetch_new_data, 'interval', seconds=30)
+        scheduler.start()
     scheduler.print_jobs()
